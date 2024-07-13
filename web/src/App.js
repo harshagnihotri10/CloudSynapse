cfff716ce563b0fc24c069bccb938711b68ed073
Harsh Agnihotri <harsh.agnihotri2020@vitbhopal.ac.in>
2024-07-13 10:21:00 +0530

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,1 +1,27 @@
 
+import React, { useEffect, useState } from 'react';
+import * as d3 from 'd3';
+
+function App() {
+  const [dependencies, setDependencies] = useState([]);
+  
+  useEffect(() => {
+    fetch('/dependencies')
+      .then(response => response.json())
+      .then(data => setDependencies(data));
+  }, []);
+  
+  useEffect(() => {
+    if (dependencies.length > 0) {
+    }
+  }, [dependencies]);
+  
+  return (
+    <div>
+      <h1>Makefile Dependency Visualizer</h1>
+      <div id="graph"></div>
+    </div>
+  );
+}
+
+export default App;
